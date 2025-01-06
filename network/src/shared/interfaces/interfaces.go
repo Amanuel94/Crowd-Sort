684198be04ca8d44963fd535e5ef07c1f1e7d00b
= <amanuel.tewodros@a2sv.org>
2025-01-06 11:22:42 +0300

    Create Global Dir

--- network/src/shared/interfaces/interfaces.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-// Comparable is an interface that defines a method to compare two values
-type Comparable[T any] interface {
-	GetIndex() any
-	GetValue() T
-	Compare(other Comparable[T]) int
-}
--- network/src/shared/types.go
+++ network/src/shared/types.go
@@ -1,69 +1,14 @@
 package shared
 
 import (
-	"network/shared/interfaces"
+	interfaces "network/globals/interfaces"
 
 	"github.com/google/uuid"
-	"golang.org/x/exp/constraints"
 )
-
-// Wrapper for indexing items
-type IndexedItem[T any] struct {
-	Index uuid.UUID // primary key
-	Value interfaces.Comparable[T]
-}
-
-func (item IndexedItem[T]) GetIndex() any {
-	return item.Index
-}
-
-func (item IndexedItem[T]) GetValue() T {
-	return item.Value.GetValue()
-}
-
-func (item IndexedItem[T]) Compare(other interfaces.Comparable[T]) int {
-	return item.Value.Compare(other)
-}
-
-func NewIndexedItem[T any](value interfaces.Comparable[T]) interfaces.Comparable[T] {
-	return IndexedItem[T]{
-		Index: uuid.New(),
-		Value: value,
-	}
-}
-
-// Wrapper for constrained types
-type OrderedType[T constraints.Ordered] struct {
-	Value T
-}
-
-func (o *OrderedType[T]) GetIndex() any {
-	return o.Value
-}
-
-func (o *OrderedType[T]) GetValue() T {
-	return o.Value
-}
-
-func (o *OrderedType[T]) Compare(other interfaces.Comparable[T]) int {
-	if o.Value < other.GetValue() {
-		return -1
-	} else if o.Value > other.GetValue() {
-		return 1
-	}
-	return 0
-}
-
-func NewInt[T constraints.Integer](value T) interfaces.Comparable[T] {
-	return &OrderedType[T]{
-		Value: value,
-	}
-}
 
 // wrapper for pair
 // ****************************************************
 
-// enum for score
 type Ord = int
 
 const (
@@ -75,7 +20,7 @@ const (
 
 type Pair[T any] struct {
 	Id    uuid.UUID
-	F     interfaces.Comparable[T]
+	F     interfaces.Comparable[T] // implements interfaces.Comparable[T]
 	S     interfaces.Comparable[T]
 	Order Ord
 }
